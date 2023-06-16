33fd0981614b381a00800e97652bd436c63d0294
Nathan Hui <nthui@eng.ucsd.edu>
2023-06-16 16:38:41 -0700

    Added hello world

--- src/smartfin-fw3.ino
+++ src/smartfin-fw3.ino
@@ -4,15 +4,17 @@
  * Author:
  * Date:
  */
+#include "Particle.h"
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
-
+    // Put initialization like pinMode and begin functions here.
+    Serial.begin(9500);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
-
+    // The core of your code will likely live here.
+    Serial.printf("Hello World!\n");
+    delay(1000);
 }
