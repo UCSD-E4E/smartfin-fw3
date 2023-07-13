74406cc8bc2f54c13c87ac01420574cc15fdcd69
Emily Thorpe <ethorpe@macalester.edu>
2023-07-12 22:19:56 -0700

    Add method to print name of current state

--- src/smartfin-fw3.ino
+++ src/smartfin-fw3.ino
@@ -1,18 +1,17 @@
 /*
  * Project smartfin-fw3
- * Description:
+ * Description: 
  * Author:
  * Date:
  */
 #include "Particle.h"
-// #include "states.hpp"
-// #include "task.hpp"
-// #include "product.hpp"
+#include "states.hpp"
+#include "task.hpp"
+#include "product.hpp"
 #include "cli/cli.hpp"
 #include "cli/conio.hpp"
 #include "cli/flog.hpp"
 
-// int counter = 0;
 
 // Statemachine for handeling task-switching
 typedef struct StateMachine_
@@ -20,11 +19,6 @@ typedef struct StateMachine_
   STATES_e state;
   Task* task;
 }StateMachine_t;
-
-static CLI cliTask;
-
-static retained uint32_t RESET_GOOD;
-static retained uint32_t nRESET_GOOD;
 
 
 // Holds the list of states and coresponding tasks
@@ -34,19 +28,21 @@ static StateMachine_t stateMachine[] =
   {STATE_NULL, NULL}
 };
 
+static CLI cliTask;
 static STATES_e currentState;
 
 static StateMachine_t* findState(STATES_e state);
 static void initalizeTaskObjects(void);
 void mainThread(void* args);
+void printState(STATES_e state);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
     Serial.begin(9600);
 
-    currentState = STATE_NULL;
+    currentState = STATE_CLI;
 
-
+    
     FLOG_Initialize();
     FLOG_AddError(FLOG_SYS_START, 0); 
 
@@ -55,35 +51,29 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-    SF_OSAL_printf("OSAL: Test");
-    // SF_OSAL_printf("Counter", counter++);
-    // Serial.print("test-serial");
-    // Serial.print("Serial: Test");
-    // FLOG_DisplayLog();
     mainThread(NULL);
 }
 
 void mainThread(void* args) {
-  StateMachine_t* pState;
-  // Starting main thread
-  while(1) 
-  {
-    SF_OSAL_printf("Starting state");
+    StateMachine_t* pState;
+    // Starting main thread
+
+    SF_OSAL_printf("Starting state \n");
     pState = findState(currentState);
-    if(NULL == pState)
-    {
-      pState = findState(STATE_NULL); 
+    SF_OSAL_printf("Current state: ");
+    printState(currentState);
+
+
+    if(pState == NULL) {
+        SF_OSAL_printf("State is null!");
+        return;
     }
-    // initalizing state
+
     pState->task->init();
 
-    // running state
     currentState = pState->task->run();
-    SF_OSAL_printf("Current State: ", currentState);
 
-    // exit state
     pState->task->exit();
-  }
 }
 
 
@@ -105,4 +95,18 @@ static StateMachine_t* findState(STATES_e state)
   }
   SF_OSAL_printf("State not found!");
   return NULL;
+}
+
+
+static void printState(STATES_e state)
+{
+  switch(state)
+  {
+    case STATE_CLI:
+    SF_OSAL_printf("STATE_CLI");
+    break;
+    default:
+    SF_OSAL_printf("UNKNOWN");
+    break;
+  }
 }
