5ba7c086cefc58a1960ff47cc9525c29a348a7ed
Nathan Hui <nthui@eng.ucsd.edu>
2023-08-03 12:46:56 -0700

    fix: Cleans up CLI output

--- src/cli/menuItems/gpsCommands.cpp
+++ src/cli/menuItems/gpsCommands.cpp
@@ -8,10 +8,11 @@
 
 void CLI_GPS() 
 {
-    LocationPoint point = {};
+    LocationPoint point;
+    LocationService& instance= LocationService::instance();
     char ch;
-    
-    while(1) 
+    int row = 0;
+    for (row = 0; ; row++)
     {
         if(kbhit()) 
         {
@@ -21,37 +22,32 @@ void CLI_GPS()
                 break;
             }
         }
-
-        LocationService::instance().getLocation(point);
-        displayInfo(point);
+        if (row % CLI_SCREEN_HEIGHT == 0)
+        {
+            SF_OSAL_printf("%11s %11s %6s %4s %6s %4s %4s %9s %9s %1s" __NL__,
+                        "Latitude",
+                        "Longitude",
+                        "Alt",
+                        "Spd",
+                        "Hdg",
+                        "HDOP",
+                        "VDOP",
+                        "Sats Used",
+                        "Sats View",
+                        "Locked");
+        }
+        instance.getLocation(point);
+        SF_OSAL_printf("%11.6f %11.6f %6.3f %4.2f %6.3f %4.2f %4.2f %9d %9d %6d" __NL__, 
+                        point.latitude,
+                        point.longitude,
+                        point.altitude,
+                        point.speed,
+                        point.heading,
+                        point.horizontalAccuracy,
+                        point.verticalAccuracy,
+                        point.satsInUse,
+                        point.satsInView,
+                        point.locked
+                        );
     }
 }
-
-
-void displayInfo(LocationPoint point)
-{
-    SF_OSAL_printf("%11s %11s %6s %6s %6s %4s %4s %9s %9s %1s",
-                "Latitude",
-                "Longitude",
-                "Alt",
-                "Speed",
-                "Hdg",
-                "HDOP",
-                "VDOP",
-                "Sats Used",
-                "Sats View",
-                "Locked");
-    SF_OSAL_printf("%11.6f %11.6f %6.3f %6.3f %6.3f %4.2f %4.2f %9d %9d %6d\r", 
-                    point.latitude,
-                    point.longitude,
-                    point.altitude,
-                    point.speed,
-                    point.heading,
-                    point.horizontalAccuracy,
-                    point.verticalAccuracy,
-                    point.satsInUse,
-                    point.satsInView,
-                    point.locked
-                    );
-}
-
