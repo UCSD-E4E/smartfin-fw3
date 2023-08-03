3889e67850367d285e748e5c2d0826230cca8c11
Emily Thorpe <ethorpe@macalester.edu>
2023-08-03 10:15:33 -0700

    Added notice of changed file

--- lib/tracker-edge/src/location_service.cpp
+++ lib/tracker-edge/src/location_service.cpp
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Modified on Auguest 2nd by @emilybthorpe
  */
 
 #include <functional>
