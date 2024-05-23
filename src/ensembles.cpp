1e93a7b4b7f226ac028a19e6bd71e00837a3eab4
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2024-05-23 13:30:25 -0700

    fix: made ensemble functions non static

--- src/ensembles.cpp
+++ src/ensembles.cpp
@@ -2,22 +2,22 @@
 #include "Particle.h"
 
 
-static void SS_ensembleAInit(DeploymentSchedule_t* pDeployment){return;}
-static void SS_ensembleAFunc(DeploymentSchedule_t* pDeployment)
+void SS_ensembleAInit(DeploymentSchedule_t* pDeployment){return;}
+void SS_ensembleAFunc(DeploymentSchedule_t* pDeployment)
 {
     delay(400);  
 }
 
 
-static void SS_ensembleBInit(DeploymentSchedule_t* pDeployment){return;}
-static void SS_ensembleBFunc(DeploymentSchedule_t* pDeployment)
+void SS_ensembleBInit(DeploymentSchedule_t* pDeployment){return;}
+void SS_ensembleBFunc(DeploymentSchedule_t* pDeployment)
 {
     delay(200);
     
 }
 
-static void SS_ensembleCInit(DeploymentSchedule_t* pDeployment){return;}
-static void SS_ensembleCFunc(DeploymentSchedule_t* pDeployment)
+void SS_ensembleCInit(DeploymentSchedule_t* pDeployment){return;}
+void SS_ensembleCFunc(DeploymentSchedule_t* pDeployment)
 {
     delay(600);
 }
--- src/ensembles.hpp
+++ src/ensembles.hpp
@@ -1,8 +1,8 @@
 #include "scheduler.hpp"
 
-static void SS_ensembleAInit(DeploymentSchedule_t* pDeployment);
-static void SS_ensembleAFunc(DeploymentSchedule_t* pDeployment);
-static void SS_ensembleBInit(DeploymentSchedule_t* pDeployment);
-static void SS_ensembleBFunc(DeploymentSchedule_t* pDeployment);
-static void SS_ensembleCInit(DeploymentSchedule_t* pDeployment);
-static void SS_ensembleCFunc(DeploymentSchedule_t* pDeployment);
+void SS_ensembleAInit(DeploymentSchedule_t* pDeployment);
+void SS_ensembleAFunc(DeploymentSchedule_t* pDeployment);
+void SS_ensembleBInit(DeploymentSchedule_t* pDeployment);
+void SS_ensembleBFunc(DeploymentSchedule_t* pDeployment);
+void SS_ensembleCInit(DeploymentSchedule_t* pDeployment);
+void SS_ensembleCFunc(DeploymentSchedule_t* pDeployment);
