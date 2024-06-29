804c44ea07bedc20f7cf9a9f4aaf1bdb25591a55
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2024-06-28 17:13:56 -0700

    fix: added build script for gtests

--- tests/README.md
+++ tests/README.md
@@ -1,59 +1,37 @@
 # Overview
 The folder `tests` contains tests monatomic tests for the scheduler in the
  `src` directory. There is a bash script to manage the tests at 
- `tests/run_test.sh`.
+ `tests/run_gtests`.
 
 # File Descriptions
-## `tests/run_test.sh` 
-This should be used to run all test cases. ithout any inputs the script runs
- two cases, a scheduler with no delays, and a scheduler with delays that have
- added that demostrate the three different cases for late ensembles. If you
- want to use custom delays, you can pass them as arguments at the end of
- run_test.sh. If you do so, it must be preceded by the number of ensembles
- to run with `-n <ensembles to run>` and optionally a title for the gantt
- chart with `-t <title>`. The gantt chartt is saved to `tests/outputs`.
+## `tests/run_gtests` 
+This should be used to run all test cases. This Bash script automates the build and testing process for the smartfin project using Google Test. It provides a command-line interface for managing compilation, testing, and graphical outputs.
 
+## Features
 
-##  `scheduler_test.cpp`
-This is equivelant to the `src/rideTask.cpp` in the main project. 
-This is where the ensembles are defined and where the scheduler is called from.
+- **Dynamic Compilation Options**: Compile with different settings like debug mode and using different compilers such as GCC or Clang.
+- **Testing**: Run specific tests passed as arguments or all tests if none are specified.
+- **Output Management**: Manage directories for output and disable graph generation if needed.
 
-## `tests/scheduler_test_system.hpp` and `tests/scheduler_test_system.cpp`
-This is where functions that are necessary for the scheduler to run are 
-redefined from the particle library.
+## Dependencies
 
-# Test case
-There are 3 ensembles: A, B, and C that run every 2000 seconds. A takes 400 ms 
-to run, B takes 200ms, C takes 600ms to run.
+- `md5sum` or `md5` for checksums to detect changes in files.
+- `cmake` and `make` for building the project.
+- `awk` for text processing within the script.
+## Options
 
-Ideal case (0ms-2000ms):
-A (0ms-400ms)
-B (500ms-700ms)
-C (800ms-1400ms)
+| Option              | Description                                                         |
+|---------------------|---------------------------------------------------------------------|
+| `-n`, `--no-graphs` | Disables the generation of graphs.|
+| `-l`, `--list-tests`| Lists all the tests configured in the test file. |
+| `-d`, `--output-dirs`| Enables output of directories where test results will be stored when run in conjuction with `-b` or `-n`|
+| `-b`, `--build-only`| Compile the project without running tests.                         |
+| `-c`, `--clang`     | Use the Clang compiler instead of GCC for compilation.            |
+| `--debug`           | Compile in debug mode, allowing for usage of gdb/lldb          |
+| `-t`, `--run-tests` | Run specific tests. This option should be followed by a list of space-separated test names to run.|
 
-Delay case 1 (2000ms-6000ms):  task A takes 200 ms longer than expected so
- task B that was supposed to run at 2500 ms is run immediately as it will not
- overlap with the next task (task C)
-A (2000ms-2600ms)
-B (2600ms-2800ms)
-C (2800ms-3400ms)
+## Usage
 
-Delay case 2 (4000ms-8000ms):  task C that starts at 4800 ms takes 800 ms
- longer so the task A that was supposed to run at 6000 ms is run at next
- available idle time as it would conflict with task B at 6500ms.
-A (4000ms-4600ms)
-B (4500ms-4700ms)
-C (4800ms-6200ms)
-
-B (6500ms-6700ms)
-C (6800ms-7400ms)
-A (7400ms-7800ms)
-
-Delay case 3 (8000ms-12000ms):  task B that starts at 8500 ms takes 800 ms 
-longer so the task C that was supposed to run at 8700 ms is not run as there
- is no idle time before task C is scheduled to run next.
-A(8000ms-8400ms)
-B(8500ms-9500ms)
-A(10000ms-10400ms)
-B(10500ms-10700ms)
-C(10800ms-11400ms)
+```bash
+./tests/run_gtests 
+```
--- tests/scheduler_proccessor.py
+++ tests/scheduler_proccessor.py
@@ -5,51 +5,95 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+import os.path
+import cv2
 
-def plot_gantt(tasks, title):
-    fig, ax = plt.subplots(figsize=(len(tasks), 3))
+
+def comparelogs(dir):
+    expected_file = open(str(dir) + "/expected.txt", "r")
+    actual_file = open(str(dir) + "/actual.txt", "r")
+    expected = expected_file.readlines()
+    actual = actual_file.readlines()
+    same = False
+    if len(expected) == len(actual):
+        same = True
+        for i in range(len(expected)):
+            if expected[i] != actual[i]:
+                same = False
+                break
+    if same:
+        tasks = parse_logs(str(dir) + "/expected.txt")
+        plot_gantt(tasks, "out", dir)
+    else:
+        
+        expected_tasks, max1 = parse_logs(str(dir) + "/expected.txt",True)
+        
+        actual_tasks, max2 = parse_logs(str(dir) + "/actual.txt",True)
+        max_duration = max(max1, max2)
+        tasks_len = max(len(actual_tasks),len(expected_tasks))
+        print(max1,max2,max_duration)
+        
+        plot_gantt(expected_tasks, "expected", dir, max_duration,tasks_len)
+        plot_gantt(actual_tasks, "actual", dir, max_duration,tasks_len)
+        img1 = cv2.imread(str(dir) + "/expected.png")
+        img2 = cv2.imread(str(dir) + "/actual.png")
+        print(img1.shape[:2])
+        print(img2.shape[:2])
+        im_v = cv2.vconcat([img1, img2]) 
+        cv2.imwrite(str(dir) + '/out.jpg', im_v)
+
+
+
+def plot_gantt(tasks, title, dir='/outputs/', max_duration=0,tasks_len=0):
+    if tasks_len == 0:
+        tasks_len = len(tasks)
+    fig, ax = plt.subplots(figsize=(tasks_len, 3))
     task_dict = {}
-
-   
     for task in tasks:
         if task['label'] not in task_dict:
             task_dict[task['label']] = []
         task_dict[task['label']].append((task['start'], task['duration']))
 
-    
     y_pos = range(len(task_dict))
-    colors = ['skyblue', 'lightgreen', 'salmon', 'grey']  
+    colors = ['skyblue', 'lightgreen', 'salmon', 'grey']
     color_idx = 0
-    
-    
+
     for i, (label, segments) in enumerate(task_dict.items()):
         last_end = 0
         for start, duration in segments:
-            
-            ax.barh(i, duration, left=start, color=colors[color_idx % len(colors)])
-            ha = 'left' if start - last_end < 300 else 'center'  
+            ax.barh(i, duration, left=start, \
+                    color=colors[color_idx % len(colors)])
+            ha = 'left' if start - last_end < 300 else 'center'
             va = 'top' if start - last_end < 300 else 'bottom'
-            
-            ax.text(start + duration / 2, i, f"{label} ({start} - {start+duration})",
-                    verticalalignment=va, horizontalalignment=ha, color='black')
+
+            # Debugging print statements
+            if len(task_dict.keys()) > 1:
+                ax.text(start + duration / 2, i, \
+                        f"{label} ({start} - {start + duration})", \
+                            verticalalignment=va, horizontalalignment=ha, \
+                                color='black')
             last_end = start + duration
         color_idx += 1
-
+    if max_duration != 0:
+        ax.set_xlim(0, max_duration)
     ax.set_yticks(y_pos)
     ax.set_yticklabels(task_dict.keys())
-    ax.set_xlabel('Time (seconds)')
+    ax.set_xlabel('Time (ms)')
     ax.set_title(title)
     ax.grid(True)
-    
+
     plt.subplots_adjust(bottom=0.15)
-    plt.savefig(str(os.path.dirname(os.path.abspath(__file__))) + '/outputs/' + title + ".png")
+    output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), \
+                               dir, title + ".png")
+    plt.savefig(output_path)
+    print(f"Plot saved to {output_path}")
 
 
-def parse_logs(filename):
+def parse_logs(filename,return_max=False):
     tasks = []
     deployment_start_time = 0
+    max_end_time = 0
     with open(filename, 'r') as file:
-        
         for line in file:
             first_line_match = re.search(r'Deployment started at (\d+)', line)
             if first_line_match:
@@ -59,20 +103,30 @@ def parse_logs(filename):
                 label = match.group(1)
                 start_time = int(match.group(2)) - deployment_start_time
                 end_time = int(match.group(3)) - deployment_start_time
-                
+
                 duration = end_time - start_time
-                tasks.append({'label': label, 'start': start_time, 'duration': duration})
+                tasks.append({'label': label, 'start': start_time, \
+                              'duration': duration})
+                if end_time > max_end_time:
+                    max_end_time = end_time
+    if return_max:
+        return tasks, max_end_time
     return tasks
 
 
-log_filename = str(os.path.dirname(os.path.abspath(__file__))) + '/outputs/log.txt'
-
-
-
-
-
-tasks = parse_logs(log_filename)
 title = "test"
+log_file = str(os.path.dirname(os.path.abspath(__file__))) + '/outputs/log.txt'
+dir_arg = False
 if len(sys.argv) > 1:
-    title = sys.argv[1]
-plot_gantt(tasks, title)
+    if os.path.isdir(sys.argv[1]):
+        dir_arg = True
+        comparelogs(sys.argv[1])
+    else:
+        log_file = str(os.path.dirname(os.path.abspath(__file__))) + \
+            '/' + sys.argv[1]
+        title = os.path.splitext(os.path.basename(log_file))[0]
+if not dir_arg:
+    print(log_file)
+    print(title)
+    tasks = parse_logs(log_file)
+    plot_gantt(tasks, title)
