aa501d9cd7cd95718d8592a71a5f189671469ba8
Nathan Hui <nthui@ucsd.edu>
2024-10-31 13:06:00 -0700

    docs: Adds file docstring

--- src/deploymentSchedule.hpp
+++ src/deploymentSchedule.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file deploymentSchedule.hpp
+ * @author Charlie Kushlevsky (ckushelevsky@ucsd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-01
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #ifndef __DEPLOYMENT_SCHDEULE_HPP__
 #define __DEPLOYMENT_SCHDEULE_HPP__
 
