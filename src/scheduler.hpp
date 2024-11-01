6fd60476024cc963681e8255fd52c949d9058ebb
Nathan Hui <nthui@ucsd.edu>
2024-10-31 21:18:42 -0700

    style: Fixes formatting and platform spec

--- src/scheduler.hpp
+++ src/scheduler.hpp
@@ -6,23 +6,20 @@
  */
 #ifndef __SCHEDULER_HPP__
 #define __SCHEDULER_HPP__
-#include "product.hpp"
-#include "deploymentSchedule.hpp"
 #include "abstractScheduler.hpp"
 #include "cli/conio.hpp"
+#include "deploymentSchedule.hpp"
+#include "product.hpp"
 
-
-#include <stddef.h>
 #include <cstdint>
 #include <queue>
+#include <stddef.h>
 
-#ifndef TEST_VERSION
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
 #include "Particle.h"
-#else
+#elif SF_PLATFORM == SF_PLATFORM_GCC
 #include "scheduler_test_system.hpp"
 #endif // TEST_VERSION
-
-
 
 /**
  * @brief Ensemble function.
@@ -34,10 +31,10 @@
  *
  * Essentially, this will be called every ensembleInterval ms after
  * ensembleDelay ms from the start of the deployment.
- * 
+ *
  * @param pDeployment the schedule table
  */
-typedef void (*EnsembleFunction)(DeploymentSchedule_t* pDeployment);
+typedef void (*EnsembleFunction)(DeploymentSchedule_t *pDeployment);
 
 /**
  * @brief Ensemble initialization function.
@@ -45,95 +42,93 @@ typedef void (*EnsembleFunction)(DeploymentSchedule_t* pDeployment);
  * This function is executed once when all of the
  * @param pDeployment the schedule table
  */
-typedef void (*EnsembleInit)(DeploymentSchedule_t* pDeployment);
+typedef void (*EnsembleInit)(DeploymentSchedule_t *pDeployment);
 
 /**
  * @brief Records and writes ensemble
  * @param pDeployment the schedule table
  */
-typedef void (*EnsembleProccess)(DeploymentSchedule_t* pDeployment);
+typedef void (*EnsembleProccess)(DeploymentSchedule_t *pDeployment);
 
 /**
-* @brief contains state information for each ensemble
-*/
+ * @brief contains state information for each ensemble
+ */
 struct StateInformation
 {
     std::uint32_t measurementCount;
     //! store the next time the task should run
     std::uint32_t nextRunTime;
-    void* pData;
-    
+    void *pData;
 };
 
-#ifndef TEST_VERSION
-#define NON_TEST_CONST const
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
+#define TESTABLE_CONST const
 #else
-#define NON_TEST_CONST
+#define TESTABLE_CONST
 #endif
 
 /**
- * @brief contains ensemble definitions and info for scheduler 
- * 
+ * @brief contains ensemble definitions and info for scheduler
+ *
  */
 struct DeploymentSchedule_
 {
     //! measurement function
-    NON_TEST_CONST EnsembleFunction measure;
-    //! initialization function              
-    NON_TEST_CONST EnsembleInit init;
+    TESTABLE_CONST EnsembleFunction measure;
+    //! initialization function
+    TESTABLE_CONST EnsembleInit init;
 
     //! measurements before processing
-    NON_TEST_CONST std::uint32_t measurementsToAccumulate; 
-    
+    TESTABLE_CONST std::uint32_t measurementsToAccumulate;
+
     //! time between ensembles
-    NON_TEST_CONST std::uint32_t ensembleInterval;              
-        
-    //! max running time of measurement 
-    NON_TEST_CONST std::uint32_t maxDuration;                 
-    
+    TESTABLE_CONST std::uint32_t ensembleInterval;
+
+    //! max running time of measurement
+    TESTABLE_CONST std::uint32_t maxDuration;
+
     //! max delay before throwing flag and resetting
-    NON_TEST_CONST std::uint32_t maxDelay; 
-    
+    TESTABLE_CONST std::uint32_t maxDelay;
+
     //! task name of ensemble
-    const char* NON_TEST_CONST taskName;                 
-    
+    const char *TESTABLE_CONST taskName;
+
     //! state information
-    StateInformation state;                
-    
+    StateInformation state;
 };
 
 /**
- * @brief contains schedule table for scheduler function to initialize and 
- * get next tasks 
- * 
+ * @brief contains schedule table for scheduler function to initialize and
+ * get next tasks
+ *
  */
-class Scheduler : public AbstractScheduler {
-    private:
+class Scheduler : public AbstractScheduler
+{
+private:
     //! schedule table size
     uint32_t tableSize;
-    public:
+
+public:
     //! the schedule table
-    DeploymentSchedule_t* scheduleTable;
-    
-    
-    
+    DeploymentSchedule_t *scheduleTable;
+
     /**
      * @brief constructor for scheduler
-     * 
+     *
      * @param schedule the schedule table
-    */
-    Scheduler(DeploymentSchedule_t* scheduler);
+     */
+    Scheduler(DeploymentSchedule_t *scheduler);
 
     /**
      * @brief Initializes ensemble variables
-     * 
+     *
      * @param scheduleTable  the schedule table
      * @param startTime the start time of the deployment
      */
     void initializeScheduler();
-    
+
     /**
-     * @brief Determines the next task to run 
+     * @brief Determines the next task to run
      *
      * @param scheduleTable The table containing all scheduled tasks.
      * @param idx The index of the current task in the schedule table.
@@ -141,13 +136,12 @@ class Scheduler : public AbstractScheduler {
      * @param nextStartTime The proposed start time of the current task.
      * @return True if there is an overlap with another task; false otherwise.
      */
-    SCH_error_e getNextTask(DeploymentSchedule_t** p_nextEvent,
-                    std::uint32_t* p_nextTime,
-                    std::uint32_t currentTime);
-    #ifdef TEST_VERSION
-    std::queue<std::tuple<const char*, std::uint32_t>> log;
-    #endif
-
+    SCH_error_e getNextTask(DeploymentSchedule_t **p_nextEvent,
+                            std::uint32_t *p_nextTime,
+                            std::uint32_t currentTime);
+#if SF_PLATFORM == SF_PLATFORM_GCC
+    std::queue<std::tuple<const char *, std::uint32_t>> log;
+#endif
 };
 
 #endif //__SCHEDULER_HPP__
