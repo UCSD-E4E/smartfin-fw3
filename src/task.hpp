32c105ee8b1d03c53943491a98a2ed6c5ac9a63d
Nathan Hui <nthui@ucsd.edu>
2024-10-31 19:56:20 -0700

    docs: Updates docstrings

--- src/task.hpp
+++ src/task.hpp
@@ -1,8 +1,25 @@
+/**
+ * @file task.hpp
+ * @author Charlie Kushulevsky (ckushelevsky@ucsd.edu)
+ * @author Nathan Hui (nthui@ucsd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2024-10-31
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #ifndef __TASK_HPP__
 #define __TASK_HPP__
 #include "states.hpp"
+/**
+ * @brief Abstract Base Class for Tasks
+ *
+ * This task is the effective abstraction of a state in a state machine.
+ *
+ */
 class Task {
-    public:
+public:
     /**
      * @brief Initializes the task.  This assumes entry from any other state.  
      * If this fails, Task::run must handle switching to the appropriate task 
@@ -11,7 +28,7 @@ class Task {
     virtual void init(void) = 0;
 
     /**
-     * @brief should be the task body.  This should execute until a state change
+     * @brief State logic body.  This should execute until a state change
      * needs to occur, in which case the state to change to should be returned.
      * Once the state to change to is returned, Task::exit will be called to
      * clean up from this state.
