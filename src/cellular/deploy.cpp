e13d7b3640d01daf41df5ef62256d382a4ef8ec5
Nathan Hui <nthui@ucsd.edu>
2024-11-14 13:49:23 -0800

    fix: Updates deploy.cpp for PC HAL

--- src/cellular/deploy.cpp
+++ src/cellular/deploy.cpp
@@ -30,6 +30,7 @@ Deployment& Deployment::getInstance(void)
 
 int Deployment::open(const char* const name, Deployment::State_e state)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (this->currentFile == 0)
     {
         ::close(this->currentFile);
@@ -57,25 +58,28 @@ int Deployment::open(const char* const name, Deployment::State_e state)
     }
     strncpy(this->filename, name, SF_NAME_MAX);
     this->currentState = state;
+#endif
     return 1;
 }
 
 int Deployment::write(void* pData, size_t nBytes)
 {
     size_t bytesWritten = 0;
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (currentFile == 0 || currentState != Deployment::WRITE)
     {
         return 0;
     }
     bytesWritten = ::write(currentFile, (uint8_t*)pData, nBytes);
     ::fsync(currentFile);
+#endif
     return bytesWritten;
 }
 
 int Deployment::read(void* pData, size_t nBytes)
 {
     size_t bytesRead = 0;
-
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (currentFile == 0)
     {
         return 0;
@@ -86,11 +90,13 @@ int Deployment::read(void* pData, size_t nBytes)
         return 0;
     }
     bytesRead = ::read(this->currentFile, (char*)pData, nBytes);
+#endif
     return bytesRead;
 }
 
 int Deployment::close(void)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (currentFile == 0)
     {
         return 1;
@@ -101,34 +107,38 @@ int Deployment::close(void)
     }
     this->currentFile = 0;
     memset(this->filename, 0, SF_NAME_MAX);
+#endif
     return 1;
 }
 
 int Deployment::seek(size_t loc)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (this->currentFile == 0)
     {
         return 0;
     }
     lseek(this->currentFile, loc, SEEK_SET);
+#endif
     return 1;
 }
 
 int Deployment::getLength(void)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (this->currentFile == 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::getLength: invalid file!" __NL__);
-    #endif
+#endif
         return 0;
     }
     struct stat sbuf = {};
     if (fstat(currentFile, &sbuf) != 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::getLength: fstat failed!" __NL__);
-    #endif
+#endif
     }
 
 #ifdef DEP_DEBUG
@@ -136,51 +146,58 @@ int Deployment::getLength(void)
 #endif
 
     return (int)sbuf.st_size;
+#else
+    return 0;
+#endif
 }
 
 int Deployment::remove(void)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (this->currentFile == 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::remove: invalid file!" __NL__);
-    #endif
+#endif
         return 0;
     }
     if (::close(this->currentFile) != 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::remove: Close failed!" __NL__);
-    #endif
+#endif
         return 0;
     }
     if (unlink(this->filename) != 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::remove: Unlink failed!" __NL__);
-    #endif
+#endif
         return 0;
     }
     memset(this->filename, 0, SF_NAME_MAX);
     this->currentFile = 0;
+#endif
     return 1;
 }
 
 int Deployment::truncate(size_t nBytes)
 {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
     if (this->currentFile == 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::truncate: invalid file!" __NL__);
-    #endif
+#endif
         return 0;
     }
     if (ftruncate(this->currentFile, nBytes) != 0)
     {
-    #ifdef DEP_DEBUG
+#ifdef DEP_DEBUG
         SF_OSAL_printf("DEP::truncate: ftruncate failed!" __NL__);
-    #endif
+#endif
         return 0;
     }
+#endif
     return 1;
 }
