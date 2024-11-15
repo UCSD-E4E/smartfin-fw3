5f752728b94adfd2ddd248f3f39ec67567b6f748
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-11-15 08:32:39 -0800

    chore: escaped special character for doxygen

--- src/cellular/encoding/base85.h
+++ src/cellular/encoding/base85.h
@@ -32,34 +32,34 @@ extern "C" {
 
 #include <stddef.h>
 
-    /** @defgroup base85 Base85 Converter.
-     * Base85 RFC 1924 version. The character set is, in order, 0–9, A–Z, a–z, and
-     * then the 23 characters !#\$%&()*+-;<=>?@^_`{|}~.
-     * @{ */
+/** @defgroup base85 Base85 Converter.
+ * Base85 RFC 1924 version. The character set is, in order, 0–9, A–Z, a–z, and
+ * then the 23 characters !#\$%&()*+-;<=>?@^_`{|}~.
+ * @{ */
 
-    /** Convert a binary memory block in a base85 null-terminated string.
-     * If the size of the source memory block is not a multiple of four,
-     * as many zeros as necessary are added to convert it to a multiple of four.
-     * @param dest Destination memory where to put the base85 null-terminated string.
-     * @param src Source binary memory block.
-     * @param size Size in bytes of source binary memory block.
-     * @return A pointer to the null character of the base85 null-terminated string. */
-    char *bintob85(char *dest, void const *src, size_t size);
+/** Convert a binary memory block in a base85 null-terminated string.
+ * If the size of the source memory block is not a multiple of four,
+ * as many zeros as necessary are added to convert it to a multiple of four.
+ * @param dest Destination memory where to put the base85 null-terminated string.
+ * @param src Source binary memory block.
+ * @param size Size in bytes of source binary memory block.
+ * @return A pointer to the null character of the base85 null-terminated string. */
+char *bintob85(char *dest, void const *src, size_t size);
 
-    /** Convert a base85 string to binary format.
-     * @param dest Destination memory block.
-     * @param src Source base85 string.
-     * @return If success a pointer to the next byte in memory block.
-     *         Null if string has a bad format.  */
-    void *b85tobin(void *dest, char const *src);
+/** Convert a base85 string to binary format.
+ * @param dest Destination memory block.
+ * @param src Source base85 string.
+ * @return If success a pointer to the next byte in memory block.
+ *         Null if string has a bad format.  */
+void *b85tobin(void *dest, char const *src);
 
-    /** Convert a base85 string to binary format.
-     * @param p Source base85 string and destination memory block.
-     * @return If success a pointer to the next byte in memory block.
-     *         Null if string has a bad format.  */
-    static inline void *b85decode(void *p)
-    {
-        return b85tobin(p, (char *)p);
+/** Convert a base85 string to binary format.
+ * @param p Source base85 string and destination memory block.
+ * @return If success a pointer to the next byte in memory block.
+ *         Null if string has a bad format.  */
+static inline void *b85decode(void *p)
+{
+    return b85tobin(p, (char *)p);
 }
 
 /** @ } */
