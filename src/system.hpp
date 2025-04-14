5a3f61771873990a160bf78db7e10e3196880581
Iha <ihagadiya@gmail.com>
2025-04-14 11:45:01 -0700

    docs: added docs for SystemDesc_ struct

--- src/system.hpp
+++ src/system.hpp
@@ -22,21 +22,69 @@ typedef volatile struct SystemFlags_
     bool inWater;
 }SystemFlags_t;
 
-
+/**
+ * @brief Contains all system components
+ */
 typedef struct SystemDesc_
 {
+    /**
+     * @brief Unique device identifier
+     */
     const char* deviceID;
+    /**
+     * @brief Pointer to Location service object that handles GPS tracking and settings
+     */
     LocationService* pLocService;
+     /**
+     * @brief Pointer toRecorder object that stores and manages data packets
+     */
     Recorder* pRecorder;
+    /**
+     * @brief Pointer to Timer object that regularly checks if device is charging
+     */
     Timer* pChargerCheck;
+    /**
+     * @brief Pointer to Timer object that regularly checks if device is in water
+     */
     Timer* pWaterCheck;
+    /**
+     * @brief Point to NVRAM object that handles non-volatile memory storage
+     * persists system state during deep sleep and power cycle
+     */
     NVRAM* pNvram;
+    /**
+     * @brief Point to WaterSensor object that detects water presence using a moving window of samples
+     */    
     WaterSensor* pWaterSensor;
+    /**
+     * @brief Pointer to Timer object that handles LED state when charging
+     * 
+     * on if charged and charger plugged in
+     * blinking if charging
+     * off if not charging
+     */
     SFLed* pBatteryLED;
+    /**
+     * @brief Pointer to Timer object that handles LED state to indicate whether in water
+     * 
+     * on if in water, off otherwise
+     */
     SFLed* pWaterLED;
+    /**
+     * @brief  Pointer to tmpSensor object that handles temperature readings and sensor initalization
+     */
     tmpSensor* pTempSensor;
+    /**
+     * @brief Pointer to LEDSystemTheme object that handles LED color and pattern settings
+     */
     LEDSystemTheme* systemTheme;
+    /**
+     * @brief Pointer to FuelGauge object that handles voltage readings
+     */
     FuelGauge* pBattery;
+    /**
+     * @brief Pointer to SystemFlags_t object that contains current system status flag
+     */
     const SystemFlags_t* flags;
 }SystemDesc_t;
 
