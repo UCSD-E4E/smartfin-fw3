3014904b915371070ffe4eba11ed8255b9e10d1c
ezh247467 <e8huang@ucsd.edu>
2025-06-01 20:37:47 -0700

    refactor: Changed conioHistory to global singleton semantics

--- pc_hal/conioHistory.cpp
+++ pc_hal/conioHistory.cpp
@@ -21,221 +21,242 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-char *mapped_memory = nullptr;
-std::size_t file_size = INITIAL_FILE_SIZE;
-std::size_t current_offset = 0;
-int fd;
-std::vector<CONIO_hist_line> Lines;
-std::vector<std::size_t> display_starts;
-std::size_t bottom_idx = 0;
-
-namespace conioHistory
+conioHistory &conioHistory::getInstance(void)
 {
-    std::size_t cur_bottom_display = 0;
-    std::size_t bottom_display = 0;
-    bool display = false;
+    static conioHistory instance;
+    return instance;
+}
 
-    void init_file_mapping()
+void conioHistory::init_file_mapping(void)
+{
+    const char *env_path = std::getenv("E4E_SF_CONIO_HISTORY");
+    env_path = env_path ? env_path : "/code/build/history.log";
+
+    fd = open(env_path, O_RDWR | O_CREAT | O_TRUNC, 0666);
+    if (fd < 0)
     {
-        const char *env_path = std::getenv("E4E_SF_CONIO_HISTORY");
-        env_path = env_path ? env_path : "/code/build/history.log";
-
-        fd = open(env_path, O_RDWR | O_CREAT | O_TRUNC, 0666);
-        if (fd < 0)
-        {
-            exit(1);
-        }
-
-        // Set initial file size
-        ftruncate(fd, INITIAL_FILE_SIZE);
-
-        // Map the file to memory
-        mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        if (mapped_memory == MAP_FAILED)
-        {
-            close(fd);
-            exit(1);
-        }
-
-        Lines.push_back(CONIO_hist_line(0)); // Initialize the first line
+        exit(1);
     }
 
-    void deinit_file_mapping()
+    // Set initial file size
+    ftruncate(fd, INITIAL_FILE_SIZE);
+
+    // Map the file to memory
+    mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (mapped_memory == MAP_FAILED)
     {
-        if (mapped_memory)
-        {
-            munmap(mapped_memory, file_size);
-        }
-        if (fd >= 0)
-        {
-            close(fd);
-        }
+        close(fd);
+        exit(1);
     }
 
-    void resize_file()
+    Lines.push_back(CONIO_hist_line(0)); // Initialize the first line
+}
+
+void conioHistory::deinit_file_mapping(void)
+{
+    if (mapped_memory)
     {
-        if (file_size >= LINEAR_STEP_RESIZE)
+        munmap(mapped_memory, file_size);
+    }
+    if (fd >= 0)
+    {
+        close(fd);
+    }
+}
+
+void conioHistory::resize_file(void)
+{
+    if (file_size >= LINEAR_STEP_RESIZE)
+    {
+        file_size += LINEAR_STEP_RESIZE;
+    }
+    else
+    {
+        file_size *= FILE_RESIZE_FACTOR;
+    }
+
+    // Resize the file on disk
+    if (ftruncate(fd, file_size) < 0)
+    {
+        close(fd);
+        exit(1);
+    }
+
+    // Unmap old memory and remap the file to the new size
+    if (munmap(mapped_memory, file_size / FILE_RESIZE_FACTOR) == -1)
+    {
+        close(fd);
+        exit(1);
+    }
+
+    mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (mapped_memory == MAP_FAILED)
+    {
+        close(fd);
+        exit(1);
+    }
+}
+
+void conioHistory::write_line(const char *line, const std::size_t size, bool NL_exists)
+{
+    // TEMPORARY: Stop writing to file at max size
+    if (current_offset + size + 2 >= MAX_FILE_SIZE)
+    {
+        return;
+    }
+    // Check if there's enough space, else resize
+    if (current_offset + size + 2 >= file_size)
+    {
+        resize_file();
+    }
+
+    // Set or create bottom line depending on display flag
+    if (display && !Lines[bottom_idx].display)
+    {
+        if (Lines[bottom_idx].len == 0)
         {
-            file_size += LINEAR_STEP_RESIZE;
+            // Default line struct converted to display line
+            Lines[bottom_idx].display = 1;
+            bottom_display = display_starts.size();
+            display_starts.push_back(bottom_idx);
+            cur_bottom_display = bottom_display;
         }
         else
         {
-            file_size *= FILE_RESIZE_FACTOR;
-        }
-
-        // Resize the file on disk
-        if (ftruncate(fd, file_size) < 0)
-        {
-            close(fd);
-            exit(1);
-        }
-
-        // Unmap old memory and remap the file to the new size
-        if (munmap(mapped_memory, file_size / FILE_RESIZE_FACTOR) == -1)
-        {
-            close(fd);
-            exit(1);
-        }
-
-        mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        if (mapped_memory == MAP_FAILED)
-        {
-            close(fd);
-            exit(1);
-        }
-    }
-
-    void write_line(const char *line, const std::size_t size, bool NL_exists)
-    {
-        // TEMPORARY: Stop writing to file at max size
-        if (current_offset + size + 2 >= MAX_FILE_SIZE)
-        {
-            return;
-        }
-        // Check if there's enough space, else resize
-        if (current_offset + size + 2 >= file_size)
-        {
-            resize_file();
-        }
-
-        // Set or create bottom line depending on display flag
-        if (display && !Lines[bottom_idx].display)
-        {
-            if (Lines[bottom_idx].len == 0)
+            int fs = 0;
+            // Check if fragmented display line exists for frag_seq num
+            std::size_t possible_frag = bottom_idx - 1;
+            if (possible_frag >= 0 && Lines[possible_frag].more_frag)
             {
-                // Default line struct converted to display line
-                Lines[bottom_idx].display = 1;
+                fs = Lines[possible_frag].frag_seq + 1;
+            }
+            // Not a fragment, update display starts
+            else
+            {
                 bottom_display = display_starts.size();
-                display_starts.push_back(bottom_idx);
+                display_starts.push_back(bottom_idx + 1);
                 cur_bottom_display = bottom_display;
             }
-            else
-            {
-                int fs = 0;
-                // Check if fragmented display line exists for frag_seq num
-                std::size_t possible_frag = bottom_idx - 1;
-                if (possible_frag >= 0 && Lines[possible_frag].more_frag)
-                {
-                    fs = Lines[possible_frag].frag_seq + 1;
-                }
-                // Not a fragment, update display starts
-                else
-                {
-                    bottom_display = display_starts.size();
-                    display_starts.push_back(bottom_idx + 1);
-                    cur_bottom_display = bottom_display;
-                }
-                Lines.push_back(CONIO_hist_line(current_offset, 0, 1, 0, fs));
-                bottom_idx++;
-            }
-        }
-        else if (!display && Lines[bottom_idx].display)
-        {
-            if (Lines[bottom_idx].len == 0)
-            {
-                // Convert empty display line to non-display line
-                Lines[bottom_idx].display = 0;
-                if (display_starts[bottom_display] == bottom_idx)
-                {
-                    display_starts.pop_back();
-                    bottom_display--;
-                }
-            }
-            else
-            {
-                // Fragment the display line
-                Lines[bottom_idx++].more_frag = 1;
-                Lines.push_back(CONIO_hist_line(current_offset));
-            }
-        }
-        strncpy(mapped_memory + current_offset, line, size);
-        current_offset += size;
-        Lines[bottom_idx].len += size;
-
-        if (display && NL_exists)
-        {
-            mapped_memory[current_offset++] = '\n';
-            Lines.push_back(CONIO_hist_line(current_offset));
+            Lines.push_back(CONIO_hist_line(current_offset, 0, 1, 0, fs));
             bottom_idx++;
         }
+    }
+    else if (!display && Lines[bottom_idx].display)
+    {
+        if (Lines[bottom_idx].len == 0)
+        {
+            // Convert empty display line to non-display line
+            Lines[bottom_idx].display = 0;
+            if (display_starts[bottom_display] == bottom_idx)
+            {
+                display_starts.pop_back();
+                bottom_display--;
+            }
+        }
+        else
+        {
+            // Fragment the display line
+            Lines[bottom_idx++].more_frag = 1;
+            Lines.push_back(CONIO_hist_line(current_offset));
+        }
+    }
+    strncpy(mapped_memory + current_offset, line, size);
+    current_offset += size;
+    Lines[bottom_idx].len += size;
 
-        msync(mapped_memory, file_size, MS_SYNC);
+    if (display && NL_exists)
+    {
+        mapped_memory[current_offset++] = '\n';
+        Lines.push_back(CONIO_hist_line(current_offset));
+        bottom_idx++;
     }
 
-    void overwrite_last_line_at(const char *line, const std::size_t size, std::size_t offset, const bool NL_exists)
-    {
-        if (offset > current_offset)
-        {
-            return; // Invalid offset
-        }
-        // Clear out the file within the interval [offset, current_offset)
-        for (std::size_t i = offset; i < current_offset; i++)
-        {
-            mapped_memory[i] = 0;
-        }
-        Lines[bottom_idx].len -= current_offset - offset;
-        current_offset = offset;
-        write_line(line, size, NL_exists);
-    }
+    msync(mapped_memory, file_size, MS_SYNC);
+}
 
-    char *retrieve_display_line(const std::size_t line_idx)
+void conioHistory::overwrite_last_line_at(const char *line, const std::size_t size, std::size_t offset, const bool NL_exists)
+{
+    if (offset > current_offset)
     {
-        // Invalid line index
-        if (line_idx >= display_starts.size())
-        {
-            return nullptr;
-        }
-        std::size_t true_idx = display_starts[line_idx]; // Grab the true index
-        if (Lines[true_idx].len == 0)
-            return nullptr;
-        std::vector<std::size_t> relevant_offsets, relevant_lens;
-        // Collect any fragments and their relevant metadata
-        while (Lines[true_idx].more_frag)
-        {
-            relevant_lens.push_back(Lines[true_idx].len);
-            relevant_offsets.push_back(Lines[true_idx].offset);
-            true_idx += 2; // Fragments only exist because true_idx + 1 is non-display line
-        }
+        return; // Invalid offset
+    }
+    // Clear out the file within the interval [offset, current_offset)
+    for (std::size_t i = offset; i < current_offset; i++)
+    {
+        mapped_memory[i] = 0;
+    }
+    Lines[bottom_idx].len -= current_offset - offset;
+    current_offset = offset;
+    write_line(line, size, NL_exists);
+}
+
+char *conioHistory::retrieve_display_line(const std::size_t line_idx)
+{
+    // Invalid line index
+    if (line_idx >= display_starts.size())
+    {
+        return nullptr;
+    }
+    std::size_t true_idx = display_starts[line_idx]; // Grab the true index
+    if (Lines[true_idx].len == 0)
+        return nullptr;
+    std::vector<std::size_t> relevant_offsets, relevant_lens;
+    // Collect any fragments and their relevant metadata
+    while (Lines[true_idx].more_frag)
+    {
         relevant_lens.push_back(Lines[true_idx].len);
         relevant_offsets.push_back(Lines[true_idx].offset);
-
-        std::size_t len = 1;
-        for (std::size_t i = 0; i < relevant_lens.size(); i++)
-            len += relevant_lens[i];
-
-        char *line = (char *)malloc(len);
-        std::size_t pos = 0;
-        for (std::size_t i = 0; i < relevant_lens.size(); i++)
-        {
-            memcpy(line + pos, mapped_memory + relevant_offsets[i], relevant_lens[i]);
-        }
-        line[len - 1] = 0;
-        return line;
+        true_idx += 2; // Fragments only exist because true_idx + 1 is non-display line
     }
+    relevant_lens.push_back(Lines[true_idx].len);
+    relevant_offsets.push_back(Lines[true_idx].offset);
 
-    std::size_t get_offset()
+    std::size_t len = 1;
+    for (std::size_t i = 0; i < relevant_lens.size(); i++)
+        len += relevant_lens[i];
+
+    char *line = (char *)malloc(len);
+    std::size_t pos = 0;
+    for (std::size_t i = 0; i < relevant_lens.size(); i++)
     {
-        return current_offset;
+        memcpy(line + pos, mapped_memory + relevant_offsets[i], relevant_lens[i]);
     }
+    line[len - 1] = 0;
+    return line;
+}
+
+std::size_t conioHistory::get_offset() const
+{
+    return current_offset;
+}
+
+bool conioHistory::get_display() const
+{
+    return display;
+}
+
+void conioHistory::set_display(bool _display)
+{
+    display = _display;
+}
+
+std::size_t conioHistory::get_bottom_display() const
+{
+    return bottom_display;
+}
+
+void conioHistory::set_bottom_display(std::size_t _bottom_display)
+{
+    bottom_display = _bottom_display;
+}
+
+std::size_t conioHistory::get_cur_bottom_display() const
+{
+    return cur_bottom_display;
+}
+
+void conioHistory::set_cur_bottom_display(std::size_t _cur_bottom_display)
+{
+    cur_bottom_display = _cur_bottom_display;
 }
 #endif
--- pc_hal/conioHistory.hpp
+++ pc_hal/conioHistory.hpp
@@ -36,129 +36,160 @@
  */
 #define MAX_FILE_SIZE 1024 * 1024 * 1024
 
-/**
- * @brief Struct that contains the necessary metadata for a line in the history
- * 
- */
-typedef struct CONIO_history_line_
+class conioHistory
 {
+    public:
+        static conioHistory &getInstance(void);
+        /**
+         * @brief Initialize memory mapped file for IO
+         *
+         */
+        void init_file_mapping(void);
+
+        /**
+         * @brief Resize the file
+         *
+         */
+        void resize_file(void);
+
+        /**
+         * @brief Deinitializes the memory mapped file
+         *
+         */
+        void deinit_file_mapping(void);
+
+        /**
+         * @brief Write the given line into the file
+         *
+         * @param line Line to be written
+         * @param size Size of the line (not counting the null terminator)
+         * @param NL_exists Add a new line after
+         */
+        void write_line(const char *line, const std::size_t size, const bool NL_exists);
+
+        /**
+         * @brief Clears the file from the given offset to EOF and writes the file at that offset
+         * @details This function is not safe to use without proper implementation knowledge and
+         * assumes that the offset provided is for the last line struct of file. Improper usage can
+         * break the history logging system
+         *
+         * @param line Line to be written
+         * @param size Size of the line (not counting the null terminator)
+         * @param offset Starting offset point to write
+         * @param NL_exists Add a new line after
+         */
+        void overwrite_last_line_at(const char *line, const std::size_t size,const std::size_t offset, const bool NL_exists);
+
+        /**
+         * @brief Retrives the display line from the given index
+         * @details This function dynamically allocates memory for the line. The user must free it
+         * after usage. A nullptr is returned if the line is empty at the index
+         *
+         * @param line_idx Index of the line to be retrieved
+         * @return char* of the line if successful
+         */
+        char *retrieve_display_line(const std::size_t line_idx);
+
+        /**
+         * @brief Get the current offset of the file
+         *
+         * @return Current offset of the file
+         */
+        std::size_t get_offset() const;
+
+        bool get_display() const;
+
+        void set_display(bool _display);
+
+        std::size_t get_bottom_display() const;
+
+        void set_bottom_display(std::size_t _bottom_display);
+
+        std::size_t get_cur_bottom_display() const;
+
+        void set_cur_bottom_display(std::size_t _cur_bottom_display);
+
+    private:
     /**
-     * @brief Starting offset within the file
+     * @brief Struct that contains the necessary metadata for a line in the history
      * 
      */
-    std::size_t offset;
-    /**
-     * @brief Length of the line
-     * 
-     */
-    std::size_t len;
-    /**
-     * @brief Display flag: 0 do not display, 1 display to window
-     * 
-     */
-    std::uint32_t display : 1;
-    /**
-     * @brief More Framgents flag: 0 no successive fragments, 1 more fragments for the line
-     * 
-     */
-    std::uint32_t more_frag : 1;
-    /**
-     * @brief Fragment sequence number within the line
-     * 
-     */
-    std::uint32_t frag_seq : 30;
-    // Additional metadata may be necessary
+    typedef struct CONIO_history_line_
+    {
+        /**
+         * @brief Starting offset within the file
+         * 
+         */
+        std::size_t offset;
+        /**
+         * @brief Length of the line
+         * 
+         */
+        std::size_t len;
+        /**
+         * @brief Display flag: 0 do not display, 1 display to window
+         * 
+         */
+        std::uint32_t display : 1;
+        /**
+         * @brief More Framgents flag: 0 no successive fragments, 1 more fragments for the line
+         * 
+         */
+        std::uint32_t more_frag : 1;
+        /**
+         * @brief Fragment sequence number within the line
+         * 
+         */
+        std::uint32_t frag_seq : 30;
+        // Additional metadata may be necessary
 
-    /**
-     * @brief Construct a new conio history line object with manually set parameters
-     * 
-     * @param _offset Starting offset
-     * @param _len Current length of the line
-     * @param _display Display flag
-     * @param _more_frag More Fragments flag
-     * @param _frag_seq Fragment sequence number
-     */
-    CONIO_history_line_(std::size_t _offset, std::size_t _len, std::uint32_t _display, std::uint32_t _more_frag, std::uint32_t _frag_seq) : offset(_offset), len(_len), display(_display), more_frag(_more_frag), frag_seq(_frag_seq) {}
-    /**
-     * @brief Construct a new conio history line object with default flags
-     * 
-     * @param _offset Starting offset
-     */
-    CONIO_history_line_(std::size_t _offset) : offset(_offset), len(0), display(0), more_frag(0), frag_seq(0) {}
-} CONIO_hist_line;
-
-namespace conioHistory
-{
-    /**
-     * @brief Initialize memory mapped file for IO
-     *
-     */
-    void init_file_mapping(void);
-
-    /**
-     * @brief Resize the file
-     *
-     */
-    void resize_file(void);
-
-    /**
-     * @brief Deinitializes the memory mapped file
-     *
-     */
-    void deinit_file_mapping(void);
-
-    /**
-     * @brief Write the given line into the file
-     *
-     * @param line Line to be written
-     * @param size Size of the line (not counting the null terminator)
-     * @param NL_exists Add a new line after
-     */
-    void write_line(const char *line, const std::size_t size, const bool NL_exists);
-
-    /**
-     * @brief Clears the file from the given offset to EOF and writes the file at that offset
-     * @details This function is not safe to use without proper implementation knowledge and assumes that the offset provided is for the last line struct of file. Improper usage can break the history logging system
-     *
-     * @param line Line to be written
-     * @param size Size of the line (not counting the null terminator)
-     * @param offset Starting offset point to write
-     * @param NL_exists Add a new line after
-     */
-    void overwrite_last_line_at(const char *line, const std::size_t size, const std::size_t offset, const bool NL_exists);
-
-    /**
-     * @brief Retrives the display line from the given index
-     * @details This function dynamically allocates memory for the line. The user must free it after usage. A nullptr is returned if the line is empty at the index
-     *
-     * @param line_idx Index of the line to be retrieved
-     * @return char* of the line if successful
-     */
-    char *retrieve_display_line(const std::size_t line_idx);
-
-    /**
-     * @brief Get the current offset of the file
-     *
-     * @return Current offset of the file
-     */
-    std::size_t get_offset();
+        /**
+         * @brief Construct a new conio history line object with manually set parameters
+         * 
+         * @param _offset Starting offset
+         * @param _len Current length of the line
+         * @param _display Display flag
+         * @param _more_frag More Fragments flag
+         * @param _frag_seq Fragment sequence number
+         */
+        CONIO_history_line_(std::size_t _offset, std::size_t _len, std::uint32_t _display, std::uint32_t _more_frag, std::uint32_t _frag_seq) : offset(_offset), len(_len), display(_display), more_frag(_more_frag), frag_seq(_frag_seq) {}
+        /**
+         * @brief Construct a new conio history line object with default flags
+         * 
+         * @param _offset Starting offset
+         */
+        CONIO_history_line_(std::size_t _offset) : offset(_offset), len(0), display(0), more_frag(0), frag_seq(0) {}
+    } CONIO_hist_line;
 
     /**
      * @brief Flag set to display line for CLI window
      * @details This flag should be set for functions that will put characters/strings into the ncurses CLI window, such as SF_OSAL_getline and SF_OSAL_printf found in conio.cpp
      */
-    extern bool display;
+    bool display = false;
 
     /**
      * @brief Keeps track of the bottom index of the CLI window
      *
      */
-    extern std::size_t cur_bottom_display;
+    std::size_t cur_bottom_display = 0;
 
     /**
      * @brief Keeps track of the bottom of the display lines
      *
      */
-    extern std::size_t bottom_display;
-}
+    std::size_t bottom_display = 0;
+
+    char *mapped_memory = nullptr;
+    std::size_t file_size = INITIAL_FILE_SIZE;
+    std::size_t current_offset = 0;
+    int fd;
+    std::vector<CONIO_hist_line> Lines;
+    std::vector<std::size_t> display_starts;
+    std::size_t bottom_idx = 0;
+
+    conioHistory() {}
+    // Disable copy constructor and assignment operator to preserve singleton nature
+    conioHistory(const conioHistory&) = delete;
+    conioHistory& operator=(const conioHistory&) = delete;
+};
 #endif
