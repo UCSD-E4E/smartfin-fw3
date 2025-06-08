c970feebfc4ac68cd4abde2515ecd7a0c159e151
ezh247467 <e8huang@ucsd.edu>
2025-06-08 07:37:46 -0700

    refactor: Added this-> to all member variables in conioHistory

--- pc_hal/conioHistory.cpp
+++ pc_hal/conioHistory.cpp
@@ -49,67 +49,67 @@ void conioHistory::init_file_mapping(void)
     const char *env_path = std::getenv("E4E_SF_CONIO_HISTORY");
     env_path = env_path ? env_path : "/code/build/history.log";
 
-    fd = open(env_path, O_RDWR | O_CREAT | O_TRUNC, 0666);
-    if (fd < 0)
+    this->fd = open(env_path, O_RDWR | O_CREAT | O_TRUNC, 0666);
+    if (this->fd < 0)
     {
         exit(1);
     }
 
     // Set initial file size
-    ftruncate(fd, INITIAL_FILE_SIZE);
+    ftruncate(this->fd, INITIAL_FILE_SIZE);
 
     // Map the file to memory
-    mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mapped_memory == MAP_FAILED)
+    this->mapped_memory = (char *)mmap(nullptr, this->file_size, PROT_READ | PROT_WRITE, MAP_SHARED, this->fd, 0);
+    if (this->mapped_memory == MAP_FAILED)
     {
-        close(fd);
+        close(this->fd);
         exit(1);
     }
 
-    lines.push_back(CONIO_hist_line(0)); // Initialize the first line
+    this->lines.push_back(CONIO_hist_line(0)); // Initialize the first line
 }
 
 void conioHistory::deinit_file_mapping(void)
 {
-    if (mapped_memory)
+    if (this->mapped_memory)
     {
-        munmap(mapped_memory, file_size);
+        munmap(this->mapped_memory, this->file_size);
     }
-    if (fd >= 0)
+    if (this->fd >= 0)
     {
-        close(fd);
+        close(this->fd);
     }
 }
 
 void conioHistory::resize_file(void)
 {
-    if (file_size >= LINEAR_STEP_RESIZE)
+    if (this->file_size >= LINEAR_STEP_RESIZE)
     {
-        file_size += LINEAR_STEP_RESIZE;
+        this->file_size += LINEAR_STEP_RESIZE;
     }
     else
     {
-        file_size *= FILE_RESIZE_FACTOR;
+        this->file_size *= FILE_RESIZE_FACTOR;
     }
 
     // Resize the file on disk
-    if (ftruncate(fd, file_size) < 0)
+    if (ftruncate(this->fd, this->file_size) < 0)
     {
-        close(fd);
+        close(this->fd);
         exit(1);
     }
 
     // Unmap old memory and remap the file to the new size
-    if (munmap(mapped_memory, file_size / FILE_RESIZE_FACTOR) == -1)
+    if (munmap(this->mapped_memory, this->file_size / FILE_RESIZE_FACTOR) == -1)
     {
-        close(fd);
+        close(this->fd);
         exit(1);
     }
 
-    mapped_memory = (char *)mmap(nullptr, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mapped_memory == MAP_FAILED)
+    this->mapped_memory = (char *)mmap(nullptr, this->file_size, PROT_READ | PROT_WRITE, MAP_SHARED, this->fd, 0);
+    if (this->mapped_memory == MAP_FAILED)
     {
-        close(fd);
+        close(this->fd);
         exit(1);
     }
 }
@@ -117,93 +117,93 @@ void conioHistory::resize_file(void)
 void conioHistory::write_line(const char *line, const std::size_t size, bool NL_exists)
 {
     // TEMPORARY: Stop writing to file at max size
-    if (current_offset + size + 2 >= MAX_FILE_SIZE)
+    if (this->current_offset + size + 2 >= MAX_FILE_SIZE)
     {
         return;
     }
     // Check if there's enough space, else resize
-    if (current_offset + size + 2 >= file_size)
+    if (this->current_offset + size + 2 >= this->file_size)
     {
         resize_file();
     }
 
     // Set or create bottom line depending on display flag
-    if (display && !lines[bottom_idx].display)
+    if (this->display && !this->lines[this->bottom_idx].display)
     {
-        if (lines[bottom_idx].len == 0)
+        if (this->lines[this->bottom_idx].len == 0)
         {
             // Default line struct converted to display line
-            lines[bottom_idx].display = true;
-            bottom_display = display_starts.size();
-            display_starts.push_back(bottom_idx);
-            cur_bottom_display = bottom_display;
+            this->lines[this->bottom_idx].display = true;
+            this->bottom_display = this->display_starts.size();
+            this->display_starts.push_back(this->bottom_idx);
+            this->cur_bottom_display = this->bottom_display;
         }
         else
         {
             int fs = 0;
             // Check if fragmented display line exists for frag_seq num
-            std::int64_t possible_frag = bottom_idx - 1;
-            if (possible_frag >= 0 && lines[possible_frag].more_frag)
+            std::int64_t possible_frag = this->bottom_idx - 1;
+            if (possible_frag >= 0 && this->lines[possible_frag].more_frag)
             {
-                fs = lines[possible_frag].frag_seq + 1;
+                fs = this->lines[possible_frag].frag_seq + 1;
             }
             // Not a fragment, update display starts
             else
             {
-                bottom_display = display_starts.size();
-                display_starts.push_back(bottom_idx + 1);
-                cur_bottom_display = bottom_display;
+                this->bottom_display = this->display_starts.size();
+                this->display_starts.push_back(this->bottom_idx + 1);
+                this->cur_bottom_display = this->bottom_display;
             }
-            lines.push_back(CONIO_hist_line(current_offset, 0, fs, true, false));
-            bottom_idx++;
+            this->lines.push_back(CONIO_hist_line(this->current_offset, 0, fs, true, false));
+            this->bottom_idx++;
         }
     }
-    else if (!display && lines[bottom_idx].display)
+    else if (!this->display && this->lines[this->bottom_idx].display)
     {
-        if (lines[bottom_idx].len == 0)
+        if (this->lines[this->bottom_idx].len == 0)
         {
             // Convert empty display line to non-display line
-            lines[bottom_idx].display = false;
-            if (display_starts[bottom_display] == bottom_idx)
+            this->lines[this->bottom_idx].display = false;
+            if (this->display_starts[this->bottom_display] == this->bottom_idx)
             {
-                display_starts.pop_back();
-                bottom_display--;
+                this->display_starts.pop_back();
+                this->bottom_display--;
             }
         }
         else
         {
             // Fragment the display line
-            lines[bottom_idx++].more_frag = true;
-            lines.push_back(CONIO_hist_line(current_offset));
+            this->lines[this->bottom_idx++].more_frag = true;
+            this->lines.push_back(CONIO_hist_line(this->current_offset));
         }
     }
-    strncpy(mapped_memory + current_offset, line, size);
-    current_offset += size;
-    lines[bottom_idx].len += size;
+    strncpy(this->mapped_memory + this->current_offset, line, size);
+    this->current_offset += size;
+    this->lines[this->bottom_idx].len += size;
 
-    if (display && NL_exists)
+    if (this->display && NL_exists)
     {
-        mapped_memory[current_offset++] = '\n';
-        lines.push_back(CONIO_hist_line(current_offset));
-        bottom_idx++;
+        this->mapped_memory[this->current_offset++] = '\n';
+        this->lines.push_back(CONIO_hist_line(this->current_offset));
+        this->bottom_idx++;
     }
 
-    msync(mapped_memory, file_size, MS_SYNC);
+    msync(this->mapped_memory, this->file_size, MS_SYNC);
 }
 
 void conioHistory::overwrite_last_line_at(const char *line, const std::size_t size, std::size_t offset, const bool NL_exists)
 {
-    if (offset > current_offset)
+    if (offset > this->current_offset)
     {
         return; // Invalid offset
     }
-    // Clear out the file within the interval [offset, current_offset)
-    for (std::size_t i = offset; i < current_offset; i++)
+    // Clear out the file within the interval [offset, this->current_offset)
+    for (std::size_t i = offset; i < this->current_offset; i++)
     {
-        mapped_memory[i] = 0;
+        this->mapped_memory[i] = 0;
     }
-    lines[bottom_idx].len -= current_offset - offset;
-    current_offset = offset;
+    this->lines[this->bottom_idx].len -= this->current_offset - offset;
+    this->current_offset = offset;
     write_line(line, size, NL_exists);
 }
 
@@ -214,19 +214,19 @@ char *conioHistory::retrieve_display_line(const std::size_t line_idx)
     {
         return nullptr;
     }
-    std::size_t true_idx = display_starts[line_idx]; // Grab the true index
-    if (lines[true_idx].len == 0)
+    std::size_t true_idx = this->display_starts[line_idx]; // Grab the true index
+    if (this->lines[true_idx].len == 0)
         return nullptr;
     std::vector<std::size_t> relevant_offsets, relevant_lens;
     // Collect any fragments and their relevant metadata
-    while (lines[true_idx].more_frag)
+    while (this->lines[true_idx].more_frag)
     {
-        relevant_lens.push_back(lines[true_idx].len);
-        relevant_offsets.push_back(lines[true_idx].offset);
+        relevant_lens.push_back(this->lines[true_idx].len);
+        relevant_offsets.push_back(this->lines[true_idx].offset);
         true_idx += 2; // Fragments only exist because true_idx + 1 is non-display line
     }
-    relevant_lens.push_back(lines[true_idx].len);
-    relevant_offsets.push_back(lines[true_idx].offset);
+    relevant_lens.push_back(this->lines[true_idx].len);
+    relevant_offsets.push_back(this->lines[true_idx].offset);
 
     std::size_t len = 1;
     for (std::size_t i = 0; i < relevant_lens.size(); i++)
@@ -236,7 +236,7 @@ char *conioHistory::retrieve_display_line(const std::size_t line_idx)
     std::size_t pos = 0;
     for (std::size_t i = 0; i < relevant_lens.size(); i++)
     {
-        memcpy(line + pos, mapped_memory + relevant_offsets[i], relevant_lens[i]);
+        memcpy(line + pos, this->mapped_memory + relevant_offsets[i], relevant_lens[i]);
         pos += relevant_lens[i];
     }
     line[len - 1] = 0;
@@ -245,36 +245,36 @@ char *conioHistory::retrieve_display_line(const std::size_t line_idx)
 
 std::size_t conioHistory::get_offset() const
 {
-    return current_offset;
+    return this->current_offset;
 }
 
 bool conioHistory::get_display() const
 {
-    return display;
+    return this->display;
 }
 
 void conioHistory::set_display(bool _display)
 {
-    display = _display;
+    this->display = _display;
 }
 
 std::size_t conioHistory::get_bottom_display() const
 {
-    return bottom_display;
+    return this->bottom_display;
 }
 
 void conioHistory::set_bottom_display(std::size_t _bottom_display)
 {
-    bottom_display = _bottom_display;
+    this->bottom_display = _bottom_display;
 }
 
 std::size_t conioHistory::get_cur_bottom_display() const
 {
-    return cur_bottom_display;
+    return this->cur_bottom_display;
 }
 
 void conioHistory::set_cur_bottom_display(std::size_t _cur_bottom_display)
 {
-    cur_bottom_display = _cur_bottom_display;
+    this->cur_bottom_display = _cur_bottom_display;
 }
 #endif
