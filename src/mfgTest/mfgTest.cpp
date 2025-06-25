f616103664d3a6514222f2ee69a98fe6bfb5ff22
Nathan Hui <nthui@ucsd.edu>
2025-06-24 20:47:44 -0700

    feat: Implements the manufacturing test

--- src/mfgTest/mfgTest.cpp
+++ src/mfgTest/mfgTest.cpp
@@ -1,39 +1,47 @@
 #include "mfgTest.hpp"
 
+#include "cellular/sf_cloud.hpp"
+#include "cli/conio.hpp"
+#include "consts.hpp"
+#include "product.hpp"
+#include "system.hpp"
+
 #include <Particle.h>
+#include <cmath>
 #include <errno.h>
 #include <stdint.h>
 
-#include "system.hpp"
-#include "cli/conio.hpp"
-#include "product.hpp"
-#include "consts.hpp"
+MfgTest::mfg_test_entry MfgTest::MFG_TEST_TABLE[] = {
+    {&MfgTest::wet_dry_sensor_test, "Wet/Dry Sensor", MfgTest::PENDING},
+    {&MfgTest::temperature_sensor_test, "Temperature Sensor", MfgTest::PENDING},
+    {&MfgTest::imu_test, "IMU", MfgTest::PENDING},
+    {&MfgTest::cellular_test, "Cellular", MfgTest::PENDING},
+    {&MfgTest::gps_test, "GPS", MfgTest::PENDING},
+    {nullptr, nullptr, MfgTest::PENDING}};
 
-const MfgTest::mfg_test_fn MfgTest::MFG_TEST_TABLE[] = {
-    &MfgTest::wet_dry_sensor_test,
-    &MfgTest::temperature_sensor_test,
-    nullptr
-};
-
-void MfgTest::init(void)
+void MfgTest::run(void)
 {
-}
-
-STATES_e MfgTest::run(void)
-{
-    const mfg_test_fn* test_fn = nullptr;
+    mfg_test_entry *test_entry = nullptr;
     String deviceID = System.deviceID();
     int retval = 0;
 
     SF_OSAL_printf("Starting Manufacturing Testing" __NL__);
     SF_OSAL_printf("Testing Device %s" __NL__, deviceID.c_str());
 
-    for(test_fn = MFG_TEST_TABLE; *test_fn; test_fn++)
+    for (test_entry = MFG_TEST_TABLE; test_entry->fn; test_entry++)
     {
-        retval |= (int)(*test_fn)();
+        test_entry->pass = (*test_entry->fn)();
     }
 
-    if(retval)
+    SF_OSAL_printf("| %24s | %16s |" __NL__, "Sensor", "Pass (0)/Fail (1)");
+    SF_OSAL_printf("|--------------------------|------------------|" __NL__);
+    for (test_entry = MFG_TEST_TABLE; test_entry->fn; test_entry++)
+    {
+        SF_OSAL_printf("| %24s | %16d |" __NL__, test_entry->name, test_entry->pass);
+        retval |= (int)(test_entry->pass);
+    }
+
+    if (retval)
     {
         SF_OSAL_printf("Manufacturing Tests FAILED" __NL__ "Mark unit as scrap." __NL__);
     }
@@ -41,11 +49,6 @@ STATES_e MfgTest::run(void)
     {
         SF_OSAL_printf("All tests passed." __NL__);
     }
-    return STATE_CLI;
-}
-
-void MfgTest::exit(void)
-{
 }
 
 MfgTest::MFG_TEST_RESULT_t MfgTest::wet_dry_sensor_test(void)
@@ -137,4 +140,144 @@ MfgTest::MFG_TEST_RESULT_t MfgTest::temperature_sensor_test(void)
     pSystemDesc->pTempSensor->stop();
 
     return retval;
+}
+
+float _std_dev(float x, float x2, std::size_t n)
+{
+    return sqrtf((n * x2 - x * x) / (n * (n - 1)));
+}
+
+void _print_axis(const char *name, float mean, float std_dev)
+{
+    SF_OSAL_printf("| %16s | %16.2f | %16.2f |" __NL__, name, mean, std_dev);
+}
+
+MfgTest::MFG_TEST_RESULT_t MfgTest::imu_test(void)
+{
+    float accel[3];
+    float rotvel[3];
+    float mag[3];
+
+    float accel_acc[3] = {0};
+    float accel_acc2[3] = {0};
+    float gyro_acc[3] = {0};
+    float gyro_acc2[3] = {0};
+    float mag_acc[3] = {0};
+    float mag_acc2[3] = {0};
+    bool fail_flag = false;
+    const std::size_t nIterations = 100;
+    SF_OSAL_printf("Running IMU Test" __NL__);
+
+    for (std::size_t idx = 0; idx < nIterations; idx++)
+    {
+#if SF_PLATFORM == SF_PLATFORM_PARTICLE
+        fail_flag |= pSystemDesc->pIMU->getDmpAccel_ms2(accel[0], accel[1], accel[2]);
+        fail_flag |= pSystemDesc->pIMU->getDmpRotVel_dps(rotvel[0], rotvel[1], rotvel[2]);
+        fail_flag |= pSystemDesc->pIMU->getDmpMag_uT(mag[0], mag[1], mag[2]);
+#endif
+
+        accel_acc[0] += accel[0];
+        accel_acc[1] += accel[1];
+        accel_acc[2] += accel[2];
+        gyro_acc[0] += rotvel[0];
+        gyro_acc[1] += rotvel[1];
+        gyro_acc[2] += rotvel[2];
+        mag_acc[0] += mag[0];
+        mag_acc[1] += mag[1];
+        mag_acc[2] += mag[2];
+
+        accel_acc2[0] += accel[0] * accel[0];
+        accel_acc2[1] += accel[1] * accel[1];
+        accel_acc2[2] += accel[2] * accel[2];
+        gyro_acc2[0] += rotvel[0] * rotvel[0];
+        gyro_acc2[1] += rotvel[1] * rotvel[1];
+        gyro_acc2[2] += rotvel[2] * rotvel[2];
+        mag_acc2[0] += mag[0] * mag[0];
+        mag_acc2[1] += mag[1] * mag[1];
+        mag_acc2[2] += mag[2] * mag[2];
+        delay(5);
+    }
+    if (fail_flag)
+    {
+        SF_OSAL_printf("IMU Test failed: Return flag" __NL__);
+        return MfgTest::FAIL;
+    }
+    float accel_mean[3];
+    float gyro_mean[3];
+    float mag_mean[3];
+
+    accel_mean[0] = accel_acc[0] / nIterations;
+    accel_mean[1] = accel_acc[1] / nIterations;
+    accel_mean[2] = accel_acc[2] / nIterations;
+    gyro_mean[0] = gyro_acc[0] / nIterations;
+    gyro_mean[1] = gyro_acc[1] / nIterations;
+    gyro_mean[2] = gyro_acc[2] / nIterations;
+    mag_mean[0] = mag_acc[0] / nIterations;
+    mag_mean[1] = mag_acc[1] / nIterations;
+    mag_mean[2] = mag_acc[2] / nIterations;
+
+    float accel_std[3], gyro_std[3], mag_std[3];
+    accel_std[0] = _std_dev(accel_acc[0], accel_acc2[0], nIterations);
+    accel_std[1] = _std_dev(accel_acc[1], accel_acc2[1], nIterations);
+    accel_std[2] = _std_dev(accel_acc[2], accel_acc2[2], nIterations);
+    gyro_std[0] = _std_dev(gyro_acc[0], gyro_acc2[0], nIterations);
+    gyro_std[1] = _std_dev(gyro_acc[1], gyro_acc2[1], nIterations);
+    gyro_std[2] = _std_dev(gyro_acc[2], gyro_acc2[2], nIterations);
+    mag_std[0] = _std_dev(mag_acc[0], mag_acc2[0], nIterations);
+    mag_std[1] = _std_dev(mag_acc[1], mag_acc2[1], nIterations);
+    mag_std[2] = _std_dev(mag_acc[2], mag_acc2[2], nIterations);
+
+    SF_OSAL_printf("| %16s | %16s | %16s |" __NL__, "Channel", "Mean", "StdDev");
+    SF_OSAL_printf("|------------------|------------------|------------------|" __NL__);
+    _print_axis("Acc.X", accel_mean[0], accel_std[0]);
+    _print_axis("Acc.Y", accel_mean[1], accel_std[1]);
+    _print_axis("Acc.Z", accel_mean[2], accel_std[2]);
+    _print_axis("Gyr.X", gyro_mean[0], gyro_std[0]);
+    _print_axis("Gyr.Y", gyro_mean[1], gyro_std[1]);
+    _print_axis("Gyr.Z", gyro_mean[2], gyro_std[2]);
+    _print_axis("Mag.X", mag_mean[0], mag_std[0]);
+    _print_axis("Mag.X", mag_mean[1], mag_std[1]);
+    _print_axis("Mag.X", mag_mean[2], mag_std[2]);
+    if (accel_mean[0] == 0 || accel_mean[1] == 0 || accel_mean[2] == 0)
+    {
+        return MfgTest::FAIL;
+    }
+    if (gyro_mean[0] == 0 || gyro_mean[1] == 0 || gyro_mean[2] == 0)
+    {
+        return MfgTest::FAIL;
+    }
+    if (mag_mean[0] == 0 || mag_mean[1] == 0 || mag_mean[2] == 0)
+    {
+        return MfgTest::FAIL;
+    }
+    return MfgTest::PASS;
+}
+
+MfgTest::MFG_TEST_RESULT_t MfgTest::cellular_test(void)
+{
+    SF_OSAL_printf("Running cellular test" __NL__);
+    sf::cloud::initialize_counter();
+    if (!sf::cloud::is_connected())
+    {
+        if (sf::cloud::wait_connect(MANUFACTURING_CELL_TIMEOUT_MS))
+        {
+            SF_OSAL_printf("Fail" __NL__);
+            return MfgTest::FAIL;
+        }
+    }
+    SF_OSAL_printf("Pass" __NL__);
+    sf::cloud::wait_disconnect(MANUFACTURING_CELL_TIMEOUT_MS);
+    return MfgTest::PASS;
+}
+
+MfgTest::MFG_TEST_RESULT_t MfgTest::gps_test(void)
+{
+    SF_OSAL_printf("Running GPS test" __NL__);
+    if (!pSystemDesc->pLocService->isActive())
+    {
+        SF_OSAL_printf("Location service is not active!" __NL__);
+        return MfgTest::FAIL;
+    }
+    SF_OSAL_printf("Location service pass" __NL__);
+    return MfgTest::PASS;
 }
--- src/mfgTest/mfgTest.hpp
+++ src/mfgTest/mfgTest.hpp
@@ -3,34 +3,30 @@
 
 #include "task.hpp"
 
-#define MFG_MIN_VALID_TEMPERATURE   -10
+#define MFG_MIN_VALID_TEMPERATURE 15
 #define MFG_MAX_VALID_TEMPERATURE   30
 
 /**
  * @brief Handles manufacturing tests
  * @details Contains methods that test the functionality of hardware components on the manufactured board
  */
-class MfgTest : public Task {
-    public:
+class MfgTest
+{
+public:
     /**
-     * @brief Initialize manufacturing test task
+     * @brief Run manufacturing tests
+     *
      */
-    void init(void);
-    /**
-     * @brief Run manufacturing tests and return to CLI
-     * 
-     * @return STATE_CLI
-     */
-    STATES_e run(void);
-    void exit(void);
+    void run(void);
 
-    private:
+private:
     /**
      * @brief Test result enumeration
-     * 
+     *
      * Defines return values for all manufacturing tests
      */
-    typedef enum {
+    typedef enum
+    {
         /**
          * @brief Return value if component passed
          */
@@ -39,35 +35,75 @@ class MfgTest : public Task {
          * @brief Return value if component failed
          */
         FAIL = 1,
+        /**
+         * @brief Component not run
+         *
+         */
+        PENDING = -1
     } MFG_TEST_RESULT_t;
     /**
      * @brief Function signature for all manufacturing tests.
-     * 
-     * All manufacturing test functions MUST return one of either MfgTest::MFG_TEST_RESULT_t::PASS or MfgTest::MFG_TEST_RESULT_t::FAIL
-     * 
-     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise MfgTest::MFG_TEST_RESULT_t::FAIL.
-     * 
+     *
+     * All manufacturing test functions MUST return one of either
+     * MfgTest::MFG_TEST_RESULT_t::PASS or MfgTest::MFG_TEST_RESULT_t::FAIL
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL.
+     *
      */
     typedef MFG_TEST_RESULT_t (*mfg_test_fn)(void);
+
+    typedef struct
+    {
+        MfgTest::mfg_test_fn fn;
+        const char *name;
+        MfgTest::MFG_TEST_RESULT_t pass;
+    } mfg_test_entry;
     /**
      * @brief Table of manufacturing tests
      * 
      * Initialized in mfgTest.cpp with references to each manufacturing test to run
      */
-    static const mfg_test_fn MFG_TEST_TABLE[];
+    static mfg_test_entry MFG_TEST_TABLE[];
 
     /**
      * @brief Test the wet/dry sensor
-     * 
-     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise MfgTest::MFG_TEST_RESULT_t::FAIL
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL
      */
     static MFG_TEST_RESULT_t wet_dry_sensor_test(void);
     /**
      * @brief Test the temperature sensor
-     * 
-     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise MfgTest::MFG_TEST_RESULT_t::FAIL
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL
      */
     static MFG_TEST_RESULT_t temperature_sensor_test(void);
+
+    /**
+     * @brief Tests the IMU
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL
+     */
+    static MFG_TEST_RESULT_t imu_test(void);
+
+    /**
+     * @brief Tests the cellular connection
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL
+     */
+    static MFG_TEST_RESULT_t cellular_test(void);
+
+    /**
+     * @brief Tests the GPS unit
+     *
+     * @return MfgTest::MFG_TEST_RESULT_t::PASS if passed, otherwise
+     * MfgTest::MFG_TEST_RESULT_t::FAIL
+     */
+    static MFG_TEST_RESULT_t gps_test(void);
 };
 
 #endif
