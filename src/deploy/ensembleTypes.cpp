ea8066727ac49c10df58eb0edac1c9824f063ca4
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-04-28 21:56:51 -0700

    feat: update sessionStart to use SF_HAL for improved hardware abstraction

--- src/deploy/ensembleTypes.cpp
+++ src/deploy/ensembleTypes.cpp
@@ -1,11 +1,12 @@
 #include "ensembleTypes.hpp"
 
-system_tick_t sessionStart = 0;
+#include "platform/hal.hpp"
+SF_HAL::tick_t sessionStart = 0;
 unsigned int Ens_getStartTime(void)
 {
-    return ((millis() - sessionStart) / 100) & 0x000FFFFF;
+    return ((SF_HAL::millis() - sessionStart) / 100) & 0x000FFFFF;
 }
 void Ens_setStartTime(void)
 {
-    sessionStart = millis();
+    sessionStart = SF_HAL::millis();
 }
