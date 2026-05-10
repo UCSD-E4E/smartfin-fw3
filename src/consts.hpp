fa087b0cbe9387b0fc150a823891bff70b75aba0
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-09 17:45:22 -0700

    refactor: standardize header guards and improve documentation for scalar definitions

--- src/consts.hpp
+++ src/consts.hpp
@@ -1,11 +1,11 @@
-#ifndef __CONSTS_HPP__
-#define __CONSTS_HPP__
+#ifndef CONSTS_HPP
+#define CONSTS_HPP
 
 #define __NL__ "\n"
 
 /**
  * @brief Unix path separator
- * 
+ *
  */
 #define PATH_SEP "/"
 
@@ -14,15 +14,31 @@
 
 /**
  * @brief Q14 Scalar
- * 
+ *
  */
 #define Q14_SCALAR 16384
+
+/**
+ * @brief Q12 Scalar
+ *
+ * Used to decode the Quat9 heading accuracy field from the DMP.
+ */
+#define Q12_SCALAR 4096
 
 /**
  * @brief Q10 Scalar
  *
  */
 #define Q10_SCALAR 1024
+
+/**
+ * @brief Q9 Scalar
+ *
+ * Correct scalar for accelerometer values in m/s² at ±4g FSR.
+ * Max representable: ±32767/512 = ±64.0 m/s²
+ */
+#define Q9_SCALAR 512
+
 /**
  * @brief Q7 Scalar
  *
@@ -31,7 +47,7 @@
 
 /**
  * @brief Q3 Scalar
- * 
+ *
  */
 #define Q3_SCALAR 8
-#endif
+#endif // CONSTS_HPP
