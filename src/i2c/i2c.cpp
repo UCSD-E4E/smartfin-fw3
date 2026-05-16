524066c7f4ff9ebe053f5d526ff5bbece05453df
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-15 20:15:20 -0700

    refactor: replace Particle I2C functions with HAL equivalents in i2c and system modules

--- src/i2c/i2c.cpp
+++ src/i2c/i2c.cpp
@@ -2,6 +2,7 @@
 
 #include "Particle.h"
 #include "cli/conio.hpp"
+#include "platform/hal.hpp"
 #include "product.hpp"
 
 #include <stdint.h>
@@ -13,53 +14,17 @@
 
 int I2C::read(uint8_t address, char *data, int length, bool repeated)
 {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
+
     // convert from 8-bit to 7-bit address
     address >>= 1;
-
-    if (!Wire.isEnabled())
-    {
-        Wire.begin();
-        delay(1000);
-    }
-
-    Wire.beginTransmission(address);
-    uint8_t bytes = Wire.requestFrom((int)address, length);
-    uint8_t idx;
-    for (idx = 0; Wire.available() && idx < bytes; idx++)
-    {
-        data[idx] = (uint8_t)Wire.read();
-    }
-    Wire.endTransmission(repeated == false);
-
-    return idx == length ? I2C_NO_ERROR : I2C_ERROR;
-#else
-    return I2C_NO_ERROR;
-#endif
+    return SF_HAL::i2c_read(address, data, length, repeated);
 }
 
 int I2C::write(uint8_t address, const char *data, int length, bool repeated)
 {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
+
     // convert from 8-bit to 7-bit address
     address >>= 1;
 
-    if (!Wire.isEnabled())
-    {
-        Wire.begin();
-        delay(1000);
-    }
-
-    Wire.beginTransmission(address);
-    uint8_t idx;
-    for (idx = 0; idx < length; idx++)
-    {
-        Wire.write(data[idx]);
-    }
-    Wire.endTransmission(repeated == false);
-
-    return idx == length ? I2C_NO_ERROR : I2C_ERROR;
-#else
-    return I2C_NO_ERROR;
-#endif
+    return SF_HAL::i2c_write(address, data, length, repeated);
 }
