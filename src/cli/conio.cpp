c8e42b6028c2d3d25f931d32293a7e5ac8c426cf
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-15 20:24:47 -0700

    refactor: replace Particle serial functions with HAL equivalents in conio module

--- src/cli/conio.cpp
+++ src/cli/conio.cpp
@@ -8,16 +8,17 @@
 
 #include "conio.hpp"
 
+#include "platform/hal.hpp"
 #include "product.hpp"
 
 #include <string>
 #if SF_PLATFORM == SF_PLATFORM_GLIBC
 #include "conioHistory.hpp"
+
+#include <cstring>
 #include <ncurses.h>
 #include <pthread.h>
-#include <cstring>
-#elif SF_PLATFORM == SF_PLATFORM_PARTICLE
-#include "Particle.h"
+#else
 #include "system.hpp"
 #endif
 
@@ -29,47 +30,48 @@ char SF_OSAL_printfBuffer[SF_OSAL_PRINTF_BUFLEN];
 #define conioHistory conioHistory::getInstance()
 /**
  * @brief Separate thread for reading in keyboard input
- * 
+ *
  */
 pthread_t read_thread;
 /**
  * @brief Length for read buffer
- * 
+ *
  */
 #define SF_OSAL_READ_BUFLEN 2048
 /**
  * @brief Buffer for keyboard input
- * 
+ *
  */
 int SF_OSAL_inputBuffer[SF_OSAL_READ_BUFLEN];
 /**
  * @brief Head points to last keyboard inputted. Tail points to last input read by the program.
- * 
+ *
  */
 std::size_t read_head_idx = 0, read_tail_idx = 0;
 /**
  * @brief Mutex used for functions accessing the inputBuffer
- * 
+ *
  */
 pthread_mutex_t read_mutex;
 /**
  * @brief Buffer used for writing to log file in SF_OSAL_getline function
- * 
+ *
  */
 std::string file_buf;
 /**
  * @brief Dimensions of the ncurses CLI window
- * 
+ *
  */
 std::size_t wind_h, wind_w;
 /**
- * @brief Flag to allow deinit_conio to distinguish if SF_OSAL_getline has already written file_buf into log file
- * 
+ * @brief Flag to allow deinit_conio to distinguish if SF_OSAL_getline has already written file_buf
+ * into log file
+ *
  */
 bool buf_written = false;
 /**
  * @brief Allows conio to know where in the log file that it may need to write to.
- * 
+ *
  */
 std::size_t offset;
 
@@ -80,7 +82,8 @@ void *read_loop(void *_)
     {
         ch = wgetch(stdscr);
         // Check for any invalid inputs
-        if (ch != '\b' && ch != KEY_BACKSPACE && ch != '\n' && ch != '\r' && (ch < ' ' || ch > '\x7F'))
+        if (ch != '\b' && ch != KEY_BACKSPACE && ch != '\n' && ch != '\r' &&
+            (ch < ' ' || ch > '\x7F'))
         {
             // Scroll inputs only matter when CLI window is active
             if (conioHistory.get_display() && conioHistory.is_active())
@@ -98,7 +101,8 @@ void *read_loop(void *_)
                     if (conioHistory.get_cur_bottom_display() == conioHistory.get_bottom_display())
                     {
                         // Save anything already written
-                        conioHistory.overwrite_last_line_at(file_buf.c_str(), file_buf.size(), offset, false);
+                        conioHistory.overwrite_last_line_at(
+                            file_buf.c_str(), file_buf.size(), offset, false);
                         buf_written = true;
                     }
                     wscrl(stdscr, -1);
@@ -129,7 +133,8 @@ void *read_loop(void *_)
                     wrefresh(stdscr);
                     conioHistory.set_cur_bottom_display(conioHistory.get_cur_bottom_display() + 1);
 
-                    char *line = conioHistory.retrieve_display_line(conioHistory.get_cur_bottom_display());
+                    char *line =
+                        conioHistory.retrieve_display_line(conioHistory.get_cur_bottom_display());
                     if (!line)
                     {
                         break;
@@ -146,7 +151,8 @@ void *read_loop(void *_)
                 }
             }
         }
-        else {
+        else
+        {
             pthread_mutex_lock(&read_mutex);
             SF_OSAL_inputBuffer[read_head_idx % SF_OSAL_READ_BUFLEN] = ch;
             read_head_idx++;
@@ -162,26 +168,21 @@ extern "C"
     // Determines if key has been pressed
     int SF_OSAL_kbhit(void)
     {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        return Serial.available();
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         int rv;
         pthread_mutex_lock(&read_mutex);
         rv = (read_head_idx != read_tail_idx);
         pthread_mutex_unlock(&read_mutex);
         return rv;
+#else
+        return SF_HAL::serial_available();
 #endif
     }
 
     int SF_OSAL_getch(void)
     {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        while (Serial.available() == 0)
-        {
-            delay(1);
-        }
-        return Serial.read();
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         // Block until there is a new input
         while (!SF_OSAL_kbhit())
         {
@@ -197,18 +198,24 @@ extern "C"
             conioHistory.write_line(retval_str, 1, false);
         }
         return retval;
+#else
+        while (SF_HAL::serial_available() == 0)
+        {
+            SF_HAL::delay_ms(1);
+        }
+        return SF_HAL::serial_read();
 #endif
     }
 
     // Write character
     int SF_OSAL_putch(int ch)
     {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        Serial.print((char)ch);
-        return ch;
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         waddch(stdscr, ch);
         wrefresh(stdscr);
+        return ch;
+#else
+        SF_HAL::serial_print_char((char)ch);
         return ch;
 #endif
     }
@@ -218,43 +225,7 @@ extern "C"
         int i = 0;
         int userInput;
 
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        while (i < buflen)
-        {
-            Particle.process();
-            if (SF_OSAL_kbhit())
-            {
-                userInput = SF_OSAL_getch();
-                switch (userInput)
-                {
-                case '\b':
-                    if (i > 0)
-                    {
-                        i--;
-                        SF_OSAL_putch('\b');
-                        SF_OSAL_putch(' ');
-                        SF_OSAL_putch('\b');
-                    }
-                    break;
-                default:
-                    buffer[i++] = userInput;
-                    SF_OSAL_putch(userInput);
-                    break;
-                case '\r':
-                    buffer[i + 1] = 0;
-                    SF_OSAL_putch('\r');
-                    SF_OSAL_putch('\n');
-                    return i;
-                }
-            }
-
-            // If we get USB disconnect, abort
-            if (!pSystemDesc->flags->hasCharger)
-            {
-                return -1;
-            }
-        }
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         conioHistory.set_display(true);
         file_buf = "";
         offset = conioHistory.get_offset();
@@ -266,11 +237,14 @@ extern "C"
 
                 buf_written = false;
                 // Move window back to the bottom first if necessary
-                if (conioHistory.get_cur_bottom_display() != conioHistory.get_bottom_display() && conioHistory.is_active())
+                if (conioHistory.get_cur_bottom_display() != conioHistory.get_bottom_display() &&
+                    conioHistory.is_active())
                 {
                     wclear(stdscr);
                     char *line;
-                    for (std::size_t idx = conioHistory.get_bottom_display() - wind_h + 1; idx < conioHistory.get_bottom_display(); idx++)
+                    for (std::size_t idx = conioHistory.get_bottom_display() - wind_h + 1;
+                         idx < conioHistory.get_bottom_display();
+                         idx++)
                     {
                         line = conioHistory.retrieve_display_line(idx);
                         if (!line)
@@ -295,28 +269,29 @@ extern "C"
                 }
                 switch (userInput)
                 {
-                    case KEY_BACKSPACE:
-                    case '\b':
-                        if (i > 0)
-                        {
-                            i--;
-                            SF_OSAL_putch('\b');
-                            SF_OSAL_putch(' ');
-                            SF_OSAL_putch('\b');
-                            file_buf.pop_back();
-                        }
-                        break;
-                    case '\n':
-                        buffer[i++] = 0;
-                        SF_OSAL_putch('\n');
-                        conioHistory.overwrite_last_line_at(file_buf.c_str(), file_buf.size(), offset, true);
-                        conioHistory.set_display(false);
-                        return i;
-                    default:
-                        buffer[i++] = userInput;
-                        SF_OSAL_putch(userInput);
-                        file_buf += userInput;
-                        break;
+                case KEY_BACKSPACE:
+                case '\b':
+                    if (i > 0)
+                    {
+                        i--;
+                        SF_OSAL_putch('\b');
+                        SF_OSAL_putch(' ');
+                        SF_OSAL_putch('\b');
+                        file_buf.pop_back();
+                    }
+                    break;
+                case '\n':
+                    buffer[i++] = 0;
+                    SF_OSAL_putch('\n');
+                    conioHistory.overwrite_last_line_at(
+                        file_buf.c_str(), file_buf.size(), offset, true);
+                    conioHistory.set_display(false);
+                    return i;
+                default:
+                    buffer[i++] = userInput;
+                    SF_OSAL_putch(userInput);
+                    file_buf += userInput;
+                    break;
                 }
             }
         }
@@ -324,26 +299,60 @@ extern "C"
         SF_OSAL_putch('\n');
         conioHistory.write_line(file_buf.c_str(), file_buf.size(), true);
         conioHistory.set_display(false);
+#else
+        while (i < buflen)
+        {
+            SF_HAL::cloud_process();
+            if (SF_OSAL_kbhit())
+            {
+                userInput = SF_OSAL_getch();
+                switch (userInput)
+                {
+                case '\b':
+                    if (i > 0)
+                    {
+                        i--;
+                        SF_OSAL_putch('\b');
+                        SF_OSAL_putch(' ');
+                        SF_OSAL_putch('\b');
+                    }
+                    break;
+                case '\r':
+                    buffer[i + 1] = 0;
+                    SF_OSAL_putch('\r');
+                    SF_OSAL_putch('\n');
+                    return i;
+                default:
+                    buffer[i++] = userInput;
+                    SF_OSAL_putch(userInput);
+                    break;
+                }
+            }
+
+            // If we get USB disconnect, abort
+            if (!pSystemDesc->flags->hasCharger)
+            {
+                return -1;
+            }
+        }
 #endif
         return i;
     }
-    
+
     // Print char array to terminal
-    int SF_OSAL_printf(const char* fmt, ...) {
+    int SF_OSAL_printf(const char *fmt, ...)
+    {
         va_list vargs;
         int nBytes = 0;
         va_start(vargs, fmt);
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        nBytes = vsnprintf(SF_OSAL_printfBuffer, SF_OSAL_PRINTF_BUFLEN, fmt, vargs);
-        Serial.write(SF_OSAL_printfBuffer);
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         conioHistory.set_display(true);
         nBytes = vsnprintf(SF_OSAL_printfBuffer, SF_OSAL_PRINTF_BUFLEN, fmt, vargs);
         wprintw(stdscr, "%s", SF_OSAL_printfBuffer);
 
-        const char* start = SF_OSAL_printfBuffer;
-        const char* newline;
-        while ((newline = strchr(start, '\n')) != NULL) 
+        const char *start = SF_OSAL_printfBuffer;
+        const char *newline;
+        while ((newline = strchr(start, '\n')) != NULL)
         {
             size_t len = newline - start;
             conioHistory.write_line(start, len, true);
@@ -351,12 +360,15 @@ extern "C"
         }
 
         // Write remaining portion after last newline (if any)
-        if (*start != '\0') 
+        if (*start != '\0')
         {
             conioHistory.write_line(start, strlen(start), false);
         }
         wrefresh(stdscr);
         conioHistory.set_display(false);
+#else
+        nBytes = vsnprintf(SF_OSAL_printfBuffer, SF_OSAL_PRINTF_BUFLEN, fmt, vargs);
+        SF_HAL::serial_write(reinterpret_cast<const uint8_t *>(SF_OSAL_printfBuffer), nBytes);
 #endif
         va_end(vargs);
         return nBytes;
@@ -364,9 +376,7 @@ extern "C"
 
     void SF_OSAL_init_conio(void)
     {
-#if SF_PLATFORM == SF_PLATFORM_PARTICLE
-        Serial.begin(SF_SERIAL_SPEED);
-#elif SF_PLATFORM == SF_PLATFORM_GLIBC
+#if SF_PLATFORM == SF_PLATFORM_GLIBC
         initscr();
         noecho();
         scrollok(stdscr, TRUE);
@@ -382,6 +392,8 @@ extern "C"
 
         pthread_create(&read_thread, NULL, read_loop, NULL);
         pthread_detach(read_thread);
+#else
+        SF_HAL::serial_begin(SF_SERIAL_SPEED);
 #endif
     }
 
